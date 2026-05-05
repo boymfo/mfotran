4e7742948f91b3d6ca436053d777378fa77e2d07
Tran Kien <boymfo@gmail.com>
2026-05-05 09:46:15 +0700

    Upload node_module

---
+++ node_modules/grapesjs-templates/locale/en.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _default = exports.default = {
+  'grapesjs-templates': {
+    projects: 'My projects',
+    templates: 'Templates',
+    'save-template': 'Save template',
+    'load-template': 'Load templates',
+    'template-settings': 'Template settings',
+    loading: 'Loading...',
+    select: 'Select',
+    'enter-template-name': 'Enter template name:',
+    'no-records': 'No projects found. Projects will appear here once you add them.'
+  }
+};
